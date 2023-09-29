9dcb2316adaebc8d1e95fc65827275875d6a05b5
Sonia quisbert <soquiro20@gmail.com>
2023-09-29 19:07:24 -0400

    api de category

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();*/
+
+    Route::apiResource('v1/categories',App\Http\Controllers\Api\V1\CategoryController::class);
+
